472f49400c37b98a8ca24cfbd38d4a985dac0087
Sanghyeon-Lee <lsh1107@kaist.ac.kr>
2020-11-23 21:20:27 +0900

    jebal work please

--- docs/js/view_logs.bcaea885.js
+++
@@ -1,2 +0,0 @@
-(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["view_logs"],{2357:function(e,l,n){"use strict";n.r(l);var t=function(){var e=this,l=e.$createElement,n=e._self._c||l;return n("p",[e._v("Hello World!")])},o=[],r=n("2877"),s={},u=Object(r["a"])(s,t,o,!1,null,null,null);l["default"]=u.exports}}]);
-//# sourceMappingURL=view_logs.bcaea885.js.map
